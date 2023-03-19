adc5412d9662e10927b6c212885c7e55f2539891
Googlefan256 <yyykakeru@gmail.com>
2023-03-19 10:53:22 +0900

    ci: 🎡 build script

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 git submodule sync
 rm -rf build
 cargo build -r --bin artifacter
-mkdir -p build
+mkdir -p build/assets
 cp target/release/artifacter build/art
+cp -r assets/trained build/assets/trained
