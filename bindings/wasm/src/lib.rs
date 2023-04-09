c8109db7bf87ffe9c0e1b1b347455bf57571a238
Googlefan256 <yyykakeru@gmail.com>
2023-04-09 20:50:10 +0900

    feat: 🎸 update wasm

--- bindings/wasm/src/lib.rs
+++ bindings/wasm/src/lib.rs
@@ -8,14 +8,16 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 use base64::{engine::general_purpose, Engine as _};
-use enkanetwork_rs::{CharacterId, EnkaNetwork, IconData};
-use gen::{generate as rs_generate, ImageFormat, Lang, ScoreCounter};
+use enkanetwork_rs::{CharacterId, EnkaNetwork, IconData, StoreValue};
+use gen::{generate as rs_generate, ImageFormat, Lang, ScoreCounter, convert};
 use once_cell::sync::OnceCell;
 use std::str::FromStr;
 
 static ENKA: OnceCell<EnkaNetwork> = OnceCell::new();
 
 static ICON_DATA: OnceCell<IconData> = OnceCell::new();
+
+static STORE: OnceCell<StoreValue> = OnceCell::new();
 
 /// Load
 #[wasm_bindgen]
@@ -25,6 +27,7 @@ pub async fn w_load() -> Result<JsValue, JsValue> {
         .await
         .map_err(|e| JsError::new(&e.to_string()))?;
     let icon_data = enka.icon_data().await;
+    let store = enka.store().await.map_err(|e| JsError::new(&e.to_string()))?;
     let e = ENKA.set(enka);
     if e.is_err() {
         return Err(JsError::new("EnkaNetwork already loaded").into());
@@ -32,6 +35,10 @@ pub async fn w_load() -> Result<JsValue, JsValue> {
     let e = ICON_DATA.set(icon_data);
     if e.is_err() {
         return Err(JsError::new("IconData already loaded").into());
+    }
+    let e = STORE.set(store);
+    if e.is_err() {
+        return Err(JsError::new("Store already loaded").into());
     }
     Ok(JsValue::UNDEFINED)
 }
@@ -41,28 +48,32 @@ pub async fn w_load() -> Result<JsValue, JsValue> {
 pub async fn get_characters(uid: i32, lang: String) -> Result<JsValue, JsValue> {
     let lang = Lang::from(lang.as_str());
     let enka = ENKA.get().ok_or(JsError::new("EnkaNetwork not loaded"))?;
+    let store = STORE.get().ok_or(JsError::new("Store not loaded"))?;
     let user = enka.simple(uid).await.map_err(|e| JsError::new(&e))?;
-    let v = &user
-        .profile()
-        .show_character_list()
-        .to_owned()
-        .iter()
-        .map(|x| {
-            (
-                x.0,
-                user.character(x.to_owned())
-                    .unwrap()
-                    .name(&enka, &lang.to_string())
-                    .unwrap_or("Unknown"),
-            )
-        })
-        .collect::<Vec<_>>();
+    let cv = user.characters_vec();
+    let mut v = vec![];
+    for x in cv {
+        let ic = x.image_icon(&enka).await;
+        let ic = ic.map(|f| convert(f, ImageFormat::Png)).flatten().unwrap_or(vec![]);
+        v.push((
+            x.id.0.clone(),
+            x
+                .name(&enka, &lang.to_string())
+                .unwrap_or("Unknown"),
+            x.level.clone(),
+            x.element.attack_name(store,&lang.to_string()),
+            general_purpose::STANDARD_NO_PAD.encode(ic.as_slice()),
+        ))
+    }
     let array = Array::new();
     for x in v {
         array.push(
             &[
                 &JsValue::from(Number::from(x.0.to_owned())),
                 &JsValue::from(JsString::from(x.1.to_owned())),
+                &JsValue::from(Number::from(x.2.to_owned())),
+                &JsValue::from(JsString::from(x.3.to_owned())),
+                &JsValue::from(JsString::from(x.4.to_owned())),
             ]
             .iter()
             .cloned()
@@ -71,6 +82,7 @@ pub async fn get_characters(uid: i32, lang: String) -> Result<JsValue, JsValue> 
     }
     Ok(array.into())
 }
+
 /// generater
 #[wasm_bindgen]
 pub async fn generate(
