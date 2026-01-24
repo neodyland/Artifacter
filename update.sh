763f29e85d3b011c0ae45b9158916005adb54a4f
googlefan256 <googlefan@neody.dev>
2026-01-24 03:04:52 +0000

    feat: Add new characters/assets

--- update.sh
+++ update.sh
@@ -6,5 +6,10 @@ curl https://github.com/EnkaNetwork/API-docs/raw/master/store/loc.json -o dynami
 jq -s '.[0] * .[1]' dynamic-assets/loc1.json dynamic-assets/loc2.json > dynamic-assets/loc.json
 rm dynamic-assets/loc1.json
 rm dynamic-assets/loc2.json
-curl https://github.com/EnkaNetwork/API-docs/raw/master/store/namecards.json -o dynamic-assets/namecards.json -L
-curl https://github.com/EnkaNetwork/API-docs/raw/master/store/characters.json -o dynamic-assets/characters.json -L
+curl https://github.com/EnkaNetwork/API-docs/raw/master/store/gi/namecards.json -o dynamic-assets/namecards.json -L
+sed -i 's/\/ui\///g' dynamic-assets/namecards.json
+sed -i 's/.jpg//g' dynamic-assets/namecards.json
+sed -i 's/Icon/icon/g' dynamic-assets/namecards.json
+curl https://github.com/EnkaNetwork/API-docs/raw/master/store/gi/avatars.json -o dynamic-assets/characters.json -L
+sed -i 's/\/ui\///g' dynamic-assets/characters.json
+sed -i 's/.png//g' dynamic-assets/characters.json
